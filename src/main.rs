7f03b88f4cae136dd10693028ff38f4073cae9c1
LordAfterEight <elias@stettmayer.de>
2025-07-03 19:05:13 +0200

    Implemented particle rendering and structs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,40 @@
-use winit::event_loop::EventLoop;
-use winit::window::Window;
-use pixels;
+use macroquad::prelude::*;
+mod world;
+use world::*;
+pub mod settings;
+use crate::settings::values::*;
 
-fn main() {
-    println!("Hello, world!");
-    let event_loop = EventLoop::new().unwrap();
-    let window_attributes = Window::default_attributes()
-        .with_title("Rusty Pixels")
-        .with_resizable(false)
-        .with_blur(true)
-        .with_transparent(true);
-    let window = event_loop.create_window(window_attributes).unwrap();
+
+#[macroquad::main("MyGame")]
+async fn main() {
+    request_new_screen_size(SCREEN_WIDTH, SCREEN_HEIGHT);
+    let mut window = macroquad::window::Conf::default();
+    window.window_resizable = false;
+    let mut world = World::new();
+
+    world.pixels.push(Pixel::new(world::Element::default(), 400.0, 0.0));
+    world.pixels.push(Pixel::new(world::Element::default(), 430.0, 45.0));
+    world.pixels.push(Pixel::new(world::Element::default(), 254.0, 20.0));
+    world.pixels.push(Pixel::new(world::Element::default(), 400.0, 90.0));
+    world.pixels.push(Pixel::new(world::Element::default(), 300.0, 23.0));
+    world.pixels.push(Pixel::new(world::Element::default(), 630.0, 73.0));
+    world.pixels.push(Pixel::new(world::Element::default(), 754.0, 57.0));
+    world.pixels.push(Pixel::new(world::Element::default(), 500.0, 47.0));
+
+    world.pixels.push(Pixel::new(
+        world::Element::new(
+            "Fire".to_string(),
+            macroquad::color::Color::new(1.0,0.6,0.0,1.0),
+            u64::MAX,
+            -0.25
+        ),
+        100.0,
+        200.0)
+    );
+
+    loop {
+        //clear_background(BLACK);
+        world.update();
+        next_frame().await
+    }
 }
